57743078ecddfab4474dfb226f5546cffbf0548e
anis_agwan <moanisagwan@applore.in>
2021-04-26 04:59:49 +0530

    Initial Commit

---
+++ Fruity/FruityApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruityApp.swift
+//  Fruity
+//
+//  Created by Anis on 26/04/21.
+//
+
+import SwiftUI
+
+@main
+struct FruityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
